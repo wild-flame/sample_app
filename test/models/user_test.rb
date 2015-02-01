579273e16d491ec131206e0e2b17af27f86705f3
wild-flame <wildflame@126.com>
2015-02-01 21:16:14 +0800

    [Lesson 6]Finish exercise

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,11 +43,18 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email should be changed into lowercase before save" do
+    mixed_email_address = "FoObAr@bar.com"
+    @user.email = mixed_email_address
+    @user.save
+    assert_equal mixed_email_address.downcase, @user.reload.email
   end
 
   test "email addresses should be unique" do
@@ -56,6 +63,7 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
